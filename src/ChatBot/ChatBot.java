6adf35c6c4b0711ea000c6b0438bb38019c51c98
m-anisimov <m.y.anisimov@student.khai.edu>
2023-09-27 13:18:05 +0300

    ChatBot 2st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,7 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is MyBot.");
+        System.out.println("I was created in 2023.");
+    }
 }
