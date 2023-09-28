8cfbdfc99ec2541168750f25b9f25e0998c10669
m-anisimov <m.y.anisimov@student.khai.edu>
2023-09-28 13:44:28 +0300

    ChatBot 3st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,7 +1,16 @@
 package ChatBot;
+
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
         System.out.println("Hello! My name is MyBot.");
         System.out.println("I was created in 2023.");
+        System.out.println("Please, remind me your name.");
+        String UserName = scn.nextLine();
+        System.out.println("What a great name you have, " + UserName + "!");
+
     }
 }
