5868af170fbe58619c025d5c7a34fc6a7b7c9267
m-anisimov <m.y.anisimov@student.khai.edu>
2023-10-04 13:27:37 +0300

    Hangman 2st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,12 @@
 package Hangman;
-
+import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("HANGMAN\n" +
+                "The game will be available soon.");
+        
 
     }
 }
