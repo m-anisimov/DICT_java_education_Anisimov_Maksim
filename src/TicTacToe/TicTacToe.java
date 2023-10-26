6bf6bb1a5196187e605fa06a2916cfda3574ab7d
m-anisimov <m.y.anisimov@student.khai.edu>
2023-10-26 22:25:51 +0300

    TicTacToe 4st stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,72 +1,107 @@
 package TicTacToe;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.math.*;
 
 public class TicTacToe {
     public static void main (String[] args) {
         Scanner scn = new Scanner(System.in);
         String s = "";
         Tic tic = new Tic();
-        System.out.println("Let's play!\nX O X\nO X O\nX X O");
 
-        while (!(s.length()==9 && (s.contains("X") || s.contains("O") || s.contains("_") ) )){
-            System.out.println("\nEnter 9 chars like: O_OXXO_XX:\n");
-            s=scn.nextLine();
+        while (!(s.length() == 9 && (s.contains("X") || s.contains("O") || s.contains("_")))) {
+            System.out.print("Enter cells (like: O_OXXO_XX): ");
+            s = scn.nextLine();
         }
-        tic.field_print(s);
+        tic.setField(new StringBuilder(s));
 
+        if (tic.getState().equals("Game not finished")) {
+            do {
+                System.out.print("Enter the coordinates (like: 1 2): ");
+                s = scn.nextLine();
+                tic.field_change(new StringBuilder(s));
+            } while(!tic.is_coord_ok());
+        }
+        System.out.println( tic.getState() );
     }
- }
+}
 
 class Tic{
-    private String hod;
+    final String[] field_coord = {"1 1", "1 2", "1 3", "2 1", "2 2", "2 3", "3 1", "3 2", "3 3"};
     private String state;
+    private StringBuilder field = new StringBuilder();
+    private char gamer;
+    private boolean coord_ok;
+
     public Tic(){
-        this.hod = hod;
+        this.gamer = 'X';
+        this.coord_ok = false;
     }
     public String getState() {
         return state;
     }
-    public void field_print(String s) {
-        String s_field;
-        char [] sc;
-        sc = s.toCharArray();
-        s_field = "";
+    public void setField(StringBuilder field) {
+        this.field = field;
+        field_print(field);
+    }
 
-        System.out.println(s);
-        for (int i = 0; i<9; i++) {
-            s_field += sc[i]  + " ";
+    public void field_change(StringBuilder xy){
+        int x, y, index;
+
+        coord_ok = false;
+        if (Arrays.toString(field_coord).contains(xy)){
+            x = Integer.parseInt(xy.substring(0, 1));
+            y = Integer.parseInt(xy.substring(2, 3));
+            index = 3 * (x - 1) + y - 1;
+
+            if (field.charAt(index) == '_') {
+                field.setCharAt(index, gamer);
+                coord_ok = true;
+                gamer = gamer == 'X' ? 'O' : 'X';
+                field_print(field);
+            }
+            else {
+                System.out.println("This cell is occupied! Choose another one!");
+            }
         }
-        System.out.println(s_field);
-        System.out.println("---------\n| " +
-                s_field.substring(0,5) + " |\n| "+
-                s_field.substring(6,11) + " |\n| "+
-                s_field.substring(12,17) + " |\n"+
-                "---------" );
+        else {
+            System.out.println("You  should  enter  numbers from 1 to 3! ");
+        }
+    }
+    private void field_print(StringBuilder s) {
+        StringBuilder s_print = new StringBuilder();
 
-        System.out.println(f_state(s));
+        for (int i = 0; i<9; i++) {
+            s_print.append(s.charAt(i)).append(" ");
+        }
+
+        System.out.println("---------\n| " +
+                s_print.substring(0,5) + " |\n| "+
+                s_print.substring(6,11) + " |\n| "+
+                s_print.substring(12,17) + " |\n"+
+                "---------" );
+        f_state(s.toString());
     }
     private String f_state(String s){
-        String rez="";
-        if ( Math.abs((f_count_xo(s,'X') - f_count_xo(s,'O') )) > 1 ) rez="Impossible";
-        else if (is_xo_win(s,'X') && is_xo_win(s,'O')) rez = "Impossible";
-        else if (is_xo_win(s,'X'))   rez = "X wins";
-        else if (is_xo_win(s,'O'))   rez = "O wins";
-        else if (s.contains("_"))  rez = "Game not finished";
-        else rez = "Draw";
-
-        return rez;
+        if ( Math.abs((f_count_xo(s,'X') - f_count_xo(s,'O') )) > 1 ) state="Impossible";
+        else if (is_xo_win(s,'X') && is_xo_win(s,'O')) state = "Impossible";
+        else if (is_xo_win(s,'X'))   state = "X wins";
+        else if (is_xo_win(s,'O'))   state = "O wins";
+        else if (s.contains("_"))  state = "Game not finished";
+        else state = "Draw";
+        return state;
     }
     private int f_count_xo(String s, char c){
         char [] sc;
         sc = s.toCharArray();
         int count=0;
 
-        for (char ch :sc ) {
+        for (char ch : sc ){
             if (ch==c) count++;
         }
-
         return count;
+    }
+    public boolean is_coord_ok(){
+        return coord_ok;
     }
     private boolean is_xo_win(String s, char xo){
         char [] sc;
@@ -79,9 +114,9 @@ class Tic{
            (sc[1]==xo && sc [4]==xo && sc[7]==xo) ||
            (sc[2]==xo && sc [5]==xo && sc[8]==xo) ||
            (sc[2]==xo && sc [4]==xo && sc[6]==xo) ||
-           (sc[0]==xo && sc [4]==xo && sc[8]==xo) )
-                return true;
-
+           (sc[0]==xo && sc [4]==xo && sc[8]==xo) ) {
+            return true;
+        }
         return false;
     }
 }
